e0b4a5cb20af4d8c1430531590afcc236b08137e
raybarkerrr <raybarkerrr@gmail.com>
2023-04-09 12:22:28 -0500

    Creating item objects
    Creating the objects needed to append to html elements so i can display the, then add them

--- javascript.js
+++ javascript.js
@@ -17,32 +17,32 @@ cartToggle.onclick = function() {
    cartMenu.classList.toggle('open');
 }
 
+let i = 0
+let time = 1000;
+let images = ["Photos/redshirt.jpeg", "Photos/blueshirt.jpeg", "Photos/greyshirt.jpeg"];
+let imageContainer = document.getElementById("image-carousel");
 
- window.initMap = initMap;
+for (let i = 0; i < images.length; i++) {
+
+  setTimeout(() => {
+    console.log(images[i]);
+  }, time * i);
+  function changeImage(){
+    document.pic.src = images[i]
+   }
+}
 
 
-
-
-
- // Carousel
- let i = 0; 
- let images = [];
- let time = 3000;
-
- images [0] = "Photos/food3.jpeg"
- images [1] = "Photos/food4.jpeg"
- images [2] = "Photos/food5.jpg"
-
- function changeImage(){
-  document.pic.src = images[i]
-
-  if(i < images.length - 1){
-    i++;
-  } else {
-    i = 0;
-  }
-
-  setTimeout("changeImage()", time)
- }
 
  window.onload=changeImage;
+
+let items = [
+  {
+    id: 1,
+    image: "Photos/redshirt.jpeg",
+    name: "Black Tee", 
+    price: 29.99,
+
+  },
+  {},
+]
